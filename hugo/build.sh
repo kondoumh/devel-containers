8f23c74164e59281db8409997566821567563172
kondoumh <kondoumh@gmail.com>
2019-09-03 03:38:05 +0000

    add site build script

--- hugo/build.sh
+++ hugo/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
 
 docker build -t hugo-builder .
+
+git clone https://github.com/kondoumh/kondoumh.com.git
+cd kondoumh.com
+git submodule update -i
+docker run --rm -it -v ${PWD}/kondoumh.com:/src -u hugo hugo-builder hugo
